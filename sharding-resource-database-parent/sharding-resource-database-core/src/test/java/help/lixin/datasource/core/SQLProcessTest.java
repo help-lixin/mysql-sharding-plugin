19025664aef4469ee156a17e88f33fdfff82acec
lixin <13798516431@126.com>
2021-11-24 21:19:06 +0800

    复杂SQL测试,验证是否影响库前缀和表前缀的替换

--- sharding-resource-database-parent/sharding-resource-database-core/src/test/java/help/lixin/datasource/core/SQLProcessTest.java
+++ sharding-resource-database-parent/sharding-resource-database-core/src/test/java/help/lixin/datasource/core/SQLProcessTest.java
@@ -12,7 +12,16 @@ public class SQLProcessTest {
 //        String sql = " SELECT * FROM user t WHERE t.id = 1 ";
 //        String sql = " INSERT INTO user(id,name,age) VALUES(?,?,?)";
 //        String sql = "  UPDATE user SET name=? WHERE id = ? ";
-        String sql = " SELECT t.* FROM user t , menu m WHERE t.id = m.u_id  AND t.id = 1 ";
+//        String sql = " SELECT t.* FROM user t , menu m WHERE t.id = m.u_id  AND t.id = 1 ";
+//         SUBQUERY QUERY
+//        String sql = "SELECT * FROM employees  WHERE birth_date >= ( SELECT MAX(birth_date) FROM user )";
+//         DERIVED  QUERY
+//        String sql = "SELECT t.*  FROM ( SELECT * FROM employees )  AS t  WHERE t.emp_no = 10034";
+//         UNION QUERY
+//        String sql = "SELECT *  FROM employees WHERE emp_no = 10034 " ;
+//        sql += " UNION ";
+//        sql += " SELECT *  FROM  employees WHERE emp_no = 10035 ";
+        String sql = " SELECT id,name,( SELECT aaa FROM user a WHERE u.id = a.id)  FROM user u  ";
         List<SQLStatement> sqlStatements = SQLUtils.parseStatements(sql, "mysql");
         MySqlASTVisitorAdapter visitor = new TestMySqlASTVisitorAdapter();
         for (SQLStatement sqlStatement : sqlStatements) {
@@ -26,7 +35,7 @@ public class SQLProcessTest {
 class TestMySqlASTVisitorAdapter extends MySqlASTVisitorAdapter {
     public boolean visit(SQLExprTableSource x) {
         String database = "db1.";
-        String tablePrefix = "t1_";
+        String tablePrefix = "tb1_";
         String tableName = x.getName().getSimpleName();
         x.setExpr(database + tablePrefix + tableName);
         return true;
